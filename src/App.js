3b7e2af6028e44114fa0ced6b4dde4f63accf818
viviancalheiros <viviancalheiros17@hotmail.com>
2023-01-16 20:26:26 -0300

    enviar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./design.css";
+import React from "react";
+import iniciar from "./iniciar.js";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="titulo perguntas">PERGUNTAS</h1>
+      <h1 className="titulo e">&</h1>
+      <h1 className="titulo respostas">RESPOSTAS</h1>
+      <button className="btn first" onClick={() => iniciar()}>
+        Iniciar jogo
+      </button>
     </div>
   );
 }
-
-export default App;
---
+++ src/iniciar.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const perguntas = [
+    "Quem foi o vencedor do bbb19?",
+    "Qual foi o primeiro animal a ir para a lua?",
+    "Qual a raiz quadrada de 576?",
+    "Quantos elementos há na tabela periódica?"
+];
+
+const q1 = [
+    ["(A) Paula", "(B) Gleici", "(C) Kaysar", "(D) Hariany"],
+    ["(A) Gato", "(B) Cachorro", "(C) Macaco", "(D) Coelho"],
+    ["(A) 13", "(B) 23", "(C) 34", "(D) 24"],
+    ["(A) 113", "(B) 109", "(C) 118", "(D) 108"]
+];
+
+const corretas = ["A", "B", "D", "C"];
+
+function iniciar() {
+    var calculo = 0;
+
+    const nome = window.prompt("Digite aqui seu nome: ");
+    window.alert("Seja bem vindo(a), " + nome + ". Vamos iniciar o jogo!");
+
+    for (let p = 0; p < perguntas.length; p++) {
+        var q = perguntas[p] + "\n";
+        for (let cont = 0; cont < q1[p].length; cont++) {
+        q += q1[p][cont] + "\n";
+    }
+    var resposta = window.prompt(q);
+    if (corretas[p].includes(resposta.toUpperCase())) {
+        calculo += 1;
+    };
+    };
+    if(calculo === 4){
+        window.alert('Você acertou todas as perguntas!');
+    }
+    else if(calculo === 1){
+        window.alert('Você fez 1 ponto!\n');
+    }
+    else if(calculo < 4 || calculo > 1){
+        window.alert('Você fez '+calculo+' pontos!\n');
+    }
+    else if(calculo === 0){
+        window.alert('Você fez 0 pontos!');
+    }
+};
+
+export default iniciar;
